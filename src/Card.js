17a62d6bfe59de0422540c89442bec52f0b7b10e
nita-kras <nita.krasnita@gmail.com>
2022-12-02 22:52:35 +0000

    cards

--- src/Card.js
+++ src/Card.js
@@ -18,13 +18,10 @@ const bull = (
       sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
     >
       •
-      component="span"
-      s2={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-      •
     </Box>
   );
   
-  export function BasicCard() {
+  export default function BasicCard() {
     return (
       <Card sx={{ minWidth: 275 }}>
         <CardContent>
@@ -60,40 +57,5 @@ const bull = (
       </Card>
       
     );
-    }
-    export function BasicCard2() {
-      return (
-        <Card s1={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            Is the colour used for aesthetics or data visualisation?
-          </Typography>
-        </CardContent>
-        <CardActions>
-        <FormControl component="fieldset">
-      <FormLabel component="legend">choose 1</FormLabel>
-      <FormGroup aria-label="position" row>
-        <FormControlLabel
-          value="Aesthetics"
-          control={<Checkbox />}
-          label="Aesthetics"
-          labelPlacement="start"
-        />
-        <FormControlLabel
-          value="Data Visualisation"
-          control={<Checkbox />}
-          label="Data Visualisation"
-          labelPlacement="start"
-        />
-        <FormControlLabel
-          value="n\a"
-          control={<Checkbox />}
-          label="n\a"
-          labelPlacement="start"
-        />
-      </FormGroup>
-    </FormControl>
-        </CardActions>
-      </Card>
-      );
   }
+   
