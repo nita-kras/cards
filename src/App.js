c33e9d065cdac72b2a15220ec7942b71c52acea4
nita-kras <nita.krasnita@gmail.com>
2022-12-02 12:24:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './App.css';
+import Figure from "./Figure";
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            figures : [],
+            currentFigureIndex: 0,
+            figuresLoaded: false
+        };
+        this.changeFigure = this.changeFigure.bind(this);
+    }
+
+    componentDidMount() {
+        fetch("https://files.catbox.moe/7dvpgw.json")
+            .then(res => res.json())
+            .then((res) => {
+                this.setState({
+                    figures: res,
+                    figuresLoaded: true
+                })
+            })
+    }
+
+    changeFigure(increment) {
+        if (increment === true) {
+            if (this.state.currentFigureIndex === 29688) {
+                this.setState({currentFigureIndex: 0});
+            } else {
+                this.setState({currentFigureIndex: this.state.currentFigureIndex + 1});
+            }
+        } else {
+            if (this.state.currentFigureIndex === 0) {
+                this.setState({currentFigureIndex: 29688});
+            } else {
+                this.setState({currentFigureIndex: this.state.currentFigureIndex - 1});
+            }
+        }
+    }
+
+    getFigureInfo(index) {
+        if (!this.state.figuresLoaded) {
+            return {name: "",
+                    doi: "",
+                    year: ""};
+        } else {
+            return {name: this.state.figures["in"][index]["name"],
+                    doi: this.state.figures["in"][index]["doi"],
+                    year: this.state.figures["in"][index]["year"]};
+        }
+    }
+
+    getImgURL(index) {
+        if (!this.state.figuresLoaded) {
+            return "https://i.imgur.com/llF5iyg.gif";
+        } else {
+            return this.state.figures["in"][index]["url"];
+        }
+    }
+
+    render() {
+        document.title = "Figure Viewer";
+        let figureInfo = this.getFigureInfo(this.state.currentFigureIndex);
+        return (
+            <div className="App">
+                <div className="metadata">
+                    <p>Title: {figureInfo.name}</p>
+                    <p>Year: {figureInfo.year}</p>
+                    <p>DOI: {figureInfo.doi}</p>
+                </div>
+                <button className="buttons" onClick={() => this.changeFigure(false)}>Previous</button>
+                <div className="Figure">
+                    <Figure imgUrl={this.getImgURL(this.state.currentFigureIndex)}/>
+                    <p id={"figure-label"}>Figure {this.state.currentFigureIndex + 1}</p>
+                </div>
+                <button className="buttons" onClick={() => this.changeFigure(true)}>Next</button>
+				<div className="questions">
+				</div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Figure.js
@@ -0,0 +1,7 @@
+function Figure({imgUrl}) {
+	return (
+		<img src={imgUrl} alt={"Figure"}/>
+	);
+}
+
+export default Figure;
