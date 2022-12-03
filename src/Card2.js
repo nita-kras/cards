d55d34c5029807ebbe30f06ece8d12225e0bdf3c
nita-kras <nita.krasnita@gmail.com>
2022-12-03 10:34:31 +0000

    cards

---
+++ src/Card2.js
@@ -0,0 +1,60 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import * as React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+
+const bull = (
+    <Box
+      component="span"
+      s2={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+      •
+    </Box>
+  );
+  
+  export default function BasicCard2() {
+    return (
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            Is the colour used for aesthetics or data visualisation?
+          </Typography>
+        </CardContent>
+        <CardActions>
+        <FormControl component="fieldset">
+      <FormLabel component="legend">choose 1</FormLabel>
+      <FormGroup aria-label="position" row>
+        <FormControlLabel
+          value="Aesthetics"
+          control={<Checkbox />}
+          label="Aesthetics"
+          labelPlacement="start"
+        />
+        <FormControlLabel
+          value="Data Visualisation"
+          control={<Checkbox />}
+          label="Data Visualisation"
+          labelPlacement="start"
+        />
+        <FormControlLabel
+          value="n\a"
+          control={<Checkbox />}
+          label="n\a"
+          labelPlacement="start"
+        />
+      </FormGroup>
+    </FormControl>
+        </CardActions>
+      </Card>
+    );
+
+   
+  }
