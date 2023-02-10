0d12366605a7c9398bee174356e24d4bb78a84e1
nita-kras <nita.krasnita@gmail.com>
2023-02-10 15:50:34 +0000

    html test

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ class App extends React.Component {
         this.changeFigure = this.changeFigure.bind(this);
     }
 
+    
+
     componentDidMount() {
         fetch("https://files.catbox.moe/7dvpgw.json")
             .then(res => res.json())
@@ -87,6 +89,25 @@ class App extends React.Component {
             </div>
         );
     }
+
+    forms(){
+        <form action="signup.html" method="post" id="signup">
+	<h1>Sign Up</h1>
+	<div class="field">
+		<label for="name">Name:</label>
+		<input type="text" id="name" name="name" placeholder="Enter your fullname" />
+		<small></small>
+	</div>
+	<div class="field">
+		<label for="email">Email:</label>
+		<input type="text" id="email" name="email" placeholder="Enter your email address" />
+		<small></small>
+	</div>
+	<button type="submit">Subscribe</button>
+</form>
+    }
+
+    
 }
 
 export default App;
