ff470c065c1c1edd0f6b2c6dc9ff69b1634135cc
nita-kras <nita.krasnita@gmail.com>
2023-02-11 14:24:17 +0000

    form test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ class App extends React.Component {
         };
         this.changeFigure = this.changeFigure.bind(this);
     }
-
     
 
     componentDidMount() {
@@ -65,6 +64,8 @@ class App extends React.Component {
         }
     }
 
+    
+
     render() {
         document.title = "Figure Viewer";
         let figureInfo = this.getFigureInfo(this.state.currentFigureIndex);
@@ -86,8 +87,19 @@ class App extends React.Component {
                 <div className = "Card">
                     <Box sx = {BasicCard()}/>
                 </div>
+                <Form>
+                    <Checkbox name="inputA1" label="Option one" defaultChecked={true} />
+                    <Checkbox name="inputA2" label="Option two" />
+                    <Checkbox name="inputA3" label="Option three is disabled" disabled={true} />
+                    <Radio name="inputB" label="Option one" defaultChecked={true} />
+                    <Radio name="inputB" label="Option two" />
+                    <Radio name="inputB" label="Option three is disabled" disabled={true} />
+                </Form>
             </div>
         );
+
+
+        
     }
 
 
--- src/saveinfo.php
+++ src/saveinfo.php
@@ -1,5 +1,5 @@
 <?php
-$imgID = $_GET['title'];
+$imgID = $_GET['currentFigureIndex'];
 $imgC= $_GET['actID'];
 $colUse = $_GET['price'];
 $mapL = $_GET['year'];
@@ -13,7 +13,6 @@ $db_name = 'psynk8_COMP2002';
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_errno)  die("failed to connect to database\n</body>\n</html>"); 
-$sql="SELECT imgID, imgColour, colourUse, mappingLegend, numColours, colourmapType, difficultyLevel FROM ImageInfo WHERE imgID='$titlesearch'";
 $add = "INSERT INTO ImageInfo (imgID, imgColour, colourUse, mappingLegend, numColours, colourmapType, difficultyLevel) VALUES ('$imgid', '$imgC', '$colUse', '$mapL', '$numC','$colMap','$diff')";
 
 ?>
