f88e02071dd5bb8e6adfaa420bb1e800fc6abc3b
nita-kras <nita.krasnita@gmail.com>
2023-02-11 13:37:06 +0000

    radio buttons

--- src/Card.js
+++ src/Card.js
@@ -10,7 +10,7 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
-import  Radio  from '@mui/material/Radio';
+import  Radio  from 'muicss/lib/react/radio';
 
 
 
