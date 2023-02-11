57375ebf67591f0be98887279d14b836c2c98c88
nita-kras <nita.krasnita@gmail.com>
2023-02-11 18:45:51 +0000

    p

--- src/App.js
+++ src/App.js
@@ -127,6 +127,24 @@ class App extends React.Component {
                     <input type = "submit" onClick={e => this.handleFormSubmit(e)} value = "Submit Annotations"/>
                 </form>
                 </div>
+                <div>
+    <form action="/action_page.php">
+    <label>First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+    <label>Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+
+
+    <label>Email</label>
+    <input type="email" id="email" name="email" placeholder="Your email" />
+
+
+    <label>Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+    <input type="submit" value="Submit" />
+    </form>
+    </div>
+   
             </div>
         );
 
